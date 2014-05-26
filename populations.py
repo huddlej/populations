42147ab1a494df8b0b52c1a43768b1be14ebf350
John Huddleston <huddlej@gmail.com>
2014-05-25 17:33:46 -0700

    moved functions into a class.

--- populations.py
+++ populations.py
@@ -4,55 +4,71 @@ from pandas import DataFrame, Series
 import random
 
 
-def create_parental_population(n, allele):
+class Population(object):
     """
-    Create a pure-breeding parental population of the given size, n, consisting
-    entirely of the given allele.
+    Genotypes are biallelic in the form of (0, 1) where 0 is a recessive allele
+    and 1 is a dominant allele. Each genotype is associated with a character of
+    the organism (e.g., seed color) and recessive and dominant phenotypes (e.g.,
+    "green" and "yellow").
     """
-    return np.array([allele] * 2*n).reshape(n, 2)
 
+    @classmethod
+    def create_parental(cls, n, allele):
+        """
+        Create a pure-breeding parental population of the given size, n, consisting
+        entirely of the given allele.
+        """
+        return cls(np.array([allele] * 2*n).reshape(n, 2))
 
-def get_alleles(population):
-    """
-    Return a random array of alleles from the given population.
-    """
-    return np.apply_along_axis(np.random.choice, 1, population)
+    def __init__(self, population):
+        """
+        Instantiate a population with an array of alleles.
+        """
+        self.population = population
 
+    def __repr__(self):
+        """
+        Return representation of the array underlying this population.
+        """
+        return repr(self.population)
 
-def cross_populations(population1, population2):
-    """
-    Sample alleles from two given populations and create a new population of the
-    same size.
-    """
-    return np.array([get_alleles(population1), get_alleles(population2)]).T
+    def get_alleles(self):
+        """
+        Return a random array of alleles from the given population.
+        """
+        return np.apply_along_axis(np.random.choice, 1, self.population)
 
+    def cross(self, other_population):
+        """
+        Create a new population from alleles sampled from this and the given
+        population.
+        """
+        return self.__class__(np.array([self.get_alleles(), other_population.get_alleles()]).T)
 
-def get_allele_frequencies(population):
-    """
-    Return the frequency of each allele in the given population.
-    """
-    unique, inverse = np.unique(population, return_inverse=True)
-    count = np.zeros(len(unique), np.int)
-    np.add.at(count, inverse, 1)
-    return count / np.float(count.sum())
+    def get_allele_frequencies(self):
+        """
+        Return the frequency of each allele in this population.
+        """
+        unique, inverse = np.unique(self.population, return_inverse=True)
+        count = np.zeros(len(unique), np.int)
+        np.add.at(count, inverse, 1)
+        return count / np.float(count.sum())
 
+    def get_expected_genotype_frequencies(self):
+        """
+        Return an array of genotype frequencies expected for this population
+        based on Hardy-Weinberg equilibrium.
+        """
+        allele_frequencies = self.get_allele_frequencies()
+        return np.array([
+            allele_frequencies[0] ** 2,
+            2 * np.product(allele_frequencies),
+            allele_frequencies[1] ** 2
+        ])
 
-def get_expected_genotype_frequencies(population):
-    """
-    Return an array of genotype frequencies expected for the given population
-    based on Hardy-Weinberg equilibrium.
-    """
-    allele_frequencies = get_allele_frequencies(population)
-    return np.array([
-        allele_frequencies[0] ** 2,
-        2 * np.product(allele_frequencies),
-        allele_frequencies[1] ** 2
-    ])
-
-
-def get_observed_genotype_frequencies(population):
-    """
-    Return the observed genotype frequencies of the given population.
-    """
-    genotypes = np.apply_along_axis(sum, 1, population)
-    return np.bincount(genotypes) / np.sum(genotypes, dtype=np.float64)
+    def get_observed_genotype_frequencies(self):
+        """
+        Return the observed genotype frequencies of this population.
+        """
+        genotypes = np.apply_along_axis(sum, 1, self.population)
+        return np.bincount(genotypes) / np.sum(genotypes, dtype=np.float64)
